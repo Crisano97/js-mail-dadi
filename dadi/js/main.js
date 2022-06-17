5139bba0230cf286dc43537d49a1aa5fa751223f
Crisano97 <97keb6@gmail.com>
2022-06-17 16:57:46 +0200

    genero un numero randomico

---
+++ dadi/js/main.js
@@ -0,0 +1,7 @@
+// Genero un numero randomico da 1 a 6, uno per il giocatore e uno per la IA;
+// Stabilisco il vincitore in base a chi fa il punteggio più alto;
+
+for (let index = 1; index <= 1; index++) {
+    index= Math.floor((Math.random() * 6) +1);
+    console.log(index)
+}
