acf04d637afdf5af8fe45c46ca4336b39f3b9b14
Crisano97 <97keb6@gmail.com>
2022-06-17 17:00:25 +0200

    genero un numero randomico per l'user e l'IA

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,7 +1,15 @@
 // Genero un numero randomico da 1 a 6, uno per il giocatore e uno per la IA;
 // Stabilisco il vincitore in base a chi fa il punteggio più alto;
+let userNumber = Math.floor((Math.random() * 6) +1);
 
 for (let index = 1; index <= 1; index++) {
-    index= Math.floor((Math.random() * 6) +1);
+    index= userNumber;
+    console.log(index)
+}
+
+let iaNumber = Math.floor((Math.random() * 6) +1);
+
+for (let index = 1; index <= 1; index++) {
+    index= iaNumber;
     console.log(index)
 }
