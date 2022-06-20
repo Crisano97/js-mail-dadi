d3148cdfe1753d5936e7f4046a9bee5ca50ba167
Crisano97 <97keb6@gmail.com>
2022-06-20 10:15:20 +0200

    corretto

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,18 +1,9 @@
 // Genero un numero randomico da 1 a 6, uno per il giocatore e uno per la IA;
 // Stabilisco il vincitore in base a chi fa il punteggio più alto;
 let userNumber = Math.floor((Math.random() * 6) +1);
-
-for (let index = 1; index <= 1; index++) {
-    index= userNumber;
-    console.log(index)
-}
-
 let iaNumber = Math.floor((Math.random() * 6) +1);
 
-for (let index = 1; index <= 1; index++) {
-    index= iaNumber;
-    console.log(index)
-}
+console.log(userNumber, iaNumber);
 
 if (userNumber > iaNumber) {
     console.warn('HAI VINTO');
